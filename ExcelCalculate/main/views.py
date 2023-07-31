d9556e08a49f3769d9b44e6a68053737f3d7daa8
anio <kitbung0214@gmail.com>
2023-07-31 16:46:14 +0900

    Edu-ExcelCal.

--- ExcelCalculate/main/views.py
+++ ExcelCalculate/main/views.py
@@ -1,3 +1,32 @@
-from django.shortcuts import render
-
+from django.shortcuts import render, redirect
+from django.http import HttpResponse
+from .models import *
 # Create your views here.
+def index(request):
+    return render(request, 'main/index.html')
+
+def signup(request):
+    return render(request, 'main/signup.html')
+
+def join(request):
+    print(request)
+    name=request.POST['signupName']
+    email=request.POST['signupEmail']
+    pw=request.POST['signupPW']
+    user=User(user_name=name, user_email=email, user_password = pw)
+    user.save()
+
+    print("사용자 정보 저장 완료됨!!")
+    return redirect('main_verifyCode')
+
+def signin(request):
+    return render(request, 'main/signin.html')
+
+def verifyCode(request):
+    return render(request, 'main/verifyCode.html')
+
+def verify(request):
+    return redirect('main_index')
+
+def result(request):
+    return render(request, 'main/result.html')
